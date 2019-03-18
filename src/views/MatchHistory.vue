50d9657cb582d6c78d4e9f43f4055046d4e297c0
amosbastian <amos_bastian@hotmail.com>
2019-03-18 19:57:57 +0100

    Add URL splitting regex

--- src/views/MatchHistory.vue
+++ src/views/MatchHistory.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="home">
-    <match-history-input
-      msg="Welcome to Your Vue.js App"
-      v-on:urlSubmitted="createThread($event)"
-    />
+    <match-history-input msg="Welcome to Your Vue.js App" v-on:urlSubmitted="createThread($event)"/>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import MatchHistoryInput from '@/components/MatchHistoryInput.vue';
 
 export default {
@@ -16,8 +14,20 @@ export default {
     MatchHistoryInput
   },
   methods: {
-    createThread(url) {
-      console.log(url);
+    async createThread(url) {
+      const baseUrl = 'https://acs.leagueoflegends.com/v1/stats/game/';
+      const pattern = /\/([A-Z\d]+)\/(\d+)\?gameHash=([a-z\d]+)/;
+      const matchingGroups = pattern.exec(url);
+
+      const platformId = matchingGroups[1];
+      const gameId = matchingGroups[2];
+      const gameHash = matchingGroups[3];
+      console.log(platformId, gameId, gameHash);
+
+      const gameUrl = `${baseUrl}${platformId}/${gameId}?gameHash=${gameHash}`;
+      const timelineUrl = `${baseUrl}${platformId}/${gameId}/timeline?gameHash=${gameHash}`;
+      console.log(gameUrl);
+      console.log(timelineUrl);
     }
   }
 };
