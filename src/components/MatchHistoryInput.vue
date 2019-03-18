c01ee8e38d715e83e869df57423dffe415ed2536
amosbastian <amos_bastian@hotmail.com>
2019-03-18 18:29:52 +0100

    Add basic project skeleton

---
+++ src/components/MatchHistoryInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container mx-auto">
+    <div class="flex justify-center py-32">
+      <div class="flex w-2/3">
+        <input
+          class="bg-grey-lighter w-full appearance-none border-2 border-grey-lighter rounded-l py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple-dark"
+          type="text"
+          placeholder="https://matchhistory.leagueoflegends.com/en/#match-details/..."
+        />
+        <button
+          class="py-2 px-4 bg-purple-dark text-white rounded-r hover:bg-purple focus:outline-none focus:border-purple"
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
