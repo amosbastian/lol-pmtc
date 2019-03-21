837fbedd7256e3f192fe978de66eabb044bd0ab7
amosbastian <amos_bastian@hotmail.com>
2019-03-21 17:29:44 +0100

    Add created hook for champions

--- src/views/MatchHistory.vue
+++ src/views/MatchHistory.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <match-history-input msg="Welcome to Your Vue.js App" v-on:urlSubmitted="createThread($event)"/>
+    <match-history-input
+      msg="Welcome to Your Vue.js App"
+      v-on:urlSubmitted="createThread($event)"
+    />
   </div>
 </template>
 
@@ -10,25 +13,54 @@ import MatchHistoryInput from '@/components/MatchHistoryInput.vue';
 
 export default {
   name: 'home',
+  data() {
+    return {
+      champions: []
+    };
+  },
   components: {
     MatchHistoryInput
   },
   methods: {
+    async handleGameData(gameUrl) {
+      const response = await axios.get(
+        `${'https://cors-anywhere.herokuapp.com/'}${gameUrl}`
+      );
+      const gameData = response.data;
+      const participantIdentities = gameData.participantIdentities;
+      console.log(participantIdentities);
+    },
     async createThread(url) {
       const baseUrl = 'https://acs.leagueoflegends.com/v1/stats/game/';
       const pattern = /\/([A-Z\d]+)\/(\d+)\?gameHash=([a-z\d]+)/;
       const matchingGroups = pattern.exec(url);
 
-      const platformId = matchingGroups[1];
-      const gameId = matchingGroups[2];
-      const gameHash = matchingGroups[3];
-      console.log(platformId, gameId, gameHash);
+      if (!matchingGroups) {
+        // TODO: handle error properly
+        alert('ERROR! ERROR! ERROR!');
+        return;
+      }
 
+      const [, platformId, gameId, gameHash] = matchingGroups;
       const gameUrl = `${baseUrl}${platformId}/${gameId}?gameHash=${gameHash}`;
       const timelineUrl = `${baseUrl}${platformId}/${gameId}/timeline?gameHash=${gameHash}`;
       console.log(gameUrl);
       console.log(timelineUrl);
+
+      this.handleGameData(gameUrl);
     }
+  },
+  async created() {
+    const versions = await axios.get(
+      'http://ddragon.leagueoflegends.com/api/versions.json'
+    );
+    const currentPatch = versions.data[0];
+    const response = await axios.get(
+      `https://ddragon.leagueoflegends.com/cdn/${currentPatch}/data/en_US/champion.json`
+    );
+    this.champions = Object.keys(response.data.data).map(
+      key => response.data.data[key]
+    );
   }
 };
 </script>
